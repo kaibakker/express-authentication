c7eeb31402a7003835d0fd2c4a9734462d2900e6
Kai Bakker <kaibakker@gmail.com>
2017-05-22 19:34:23 +0200

    Fixed startup of new tasks

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.post('/communities', isLoggedIn, function(req, res) {
     user.communities.push(community)
     user.save(function(err) {
       if(!err) {
-        request('http://launcher.enterslack.com/communities/' + community.slack_subdomain + '/statup', function (error, response, body) {
+        request('http://launcher.enterslack.com/communities/' + community.slack_subdomain + '/startup', function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
